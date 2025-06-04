152b2a07c7f9a2bb6d10498b49cae533640982c5
Muhammed Karamuk <mkaramuk@proton.me>
2025-06-04 06:33:39 +0300

    fix: auto refresh Supabase token

--- src/uploaders/PeerBench.ts
+++ src/uploaders/PeerBench.ts
@@ -1,8 +1,9 @@
 import { AbstractUploader, UploadAuditFile } from "@/base/AbstractUploader";
 import { config } from "@/core/config";
 import { Validator } from "@/core/validator";
+import { sleep } from "@/utils/sleep";
 import { calculateSHA256 } from "@forest-protocols/sdk";
-import { createClient, SupabaseClient } from "@supabase/supabase-js";
+import { createClient, Session, SupabaseClient } from "@supabase/supabase-js";
 import axios from "axios";
 
 /**
@@ -12,6 +13,11 @@ export class PeerBenchUploader extends AbstractUploader {
   token?: string;
   supabaseClient?: SupabaseClient;
   apiURL: string;
+  session: Session | null = null;
+
+  private refreshTokenInterval?: NodeJS.Timeout;
+  private isRefreshingToken = false;
+  private isClosed = false;
 
   constructor(validator: Validator) {
     super(validator);
@@ -24,8 +30,7 @@ export class PeerBenchUploader extends AbstractUploader {
       config.PEERBENCH_UPLOADER_SUPABASE_ANON_KEY ||
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ3bnNlcmJqanR4Z2theWRrbXRrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU5OTczMDQsImV4cCI6MjA2MTU3MzMwNH0.hzPhuPtDYF1yMrKseoZX-P6ETpPEsGIC8mt-bieG6Kc"
     );
-    this.apiURL =
-      config.PEERBENCH_API_URL || "https://dev-peer-bench-js.vercel.app/api";
+    this.apiURL = config.PEERBENCH_API_URL || "https://peerbench.ai/api";
   }
 
   async init(): Promise<void> {
@@ -40,9 +45,12 @@ export class PeerBenchUploader extends AbstractUploader {
       this.validator.actorInfo.ownerAddr + privateKey
     );
 
-    this.token =
+    const authData =
       (await this.login(email, password)) ||
       (await this.signUp(email, password));
+
+    this.session = authData?.session || null;
+    this.token = this.session?.access_token;
 
     if (!this.token) {
       this.logger.error(
@@ -52,9 +60,50 @@ export class PeerBenchUploader extends AbstractUploader {
     }
     this.logger.info(`Authenticated with PeerBench successfully`);
 
-    // Start auto refreshing the token
-    await this.supabaseClient.auth.startAutoRefresh();
-    this.logger.info(`Started auto refreshing Supabase token`);
+    // Refresh the token 15 minutes before it expires
+    this.refreshTokenInterval = setInterval(
+      () => this.refreshToken(),
+      this.session!.expires_in - 15 * 60 * 1000
+    );
+  }
+
+  async refreshToken() {
+    if (this.isClosed) {
+      return;
+    }
+
+    if (this.isRefreshingToken) {
+      this.logger.debug(`Token is already being refreshed`);
+      return;
+    }
+
+    if (!this.supabaseClient) {
+      clearInterval(this.refreshTokenInterval!);
+      return;
+    }
+
+    this.isRefreshingToken = true;
+    this.logger.info(`Refreshing token`);
+    while (!this.isClosed) {
+      try {
+        const { data, error } = await this.supabaseClient.auth.refreshSession(
+          this.session || undefined
+        );
+        if (error) {
+          throw new Error(error.message);
+        }
+
+        this.session = data.session;
+        break;
+      } catch (err) {
+        this.logger.debug(
+          `Failed to refresh Supabase token: ${JSON.stringify(err, null, 2)}`
+        );
+        this.logger.debug(`Retrying in 10 seconds`);
+        await sleep(10_000);
+      }
+    }
+    this.isRefreshingToken = false;
   }
 
   async upload(auditFiles: UploadAuditFile[]): Promise<void> {
@@ -97,8 +146,12 @@ export class PeerBenchUploader extends AbstractUploader {
     if (!this.supabaseClient) {
       return;
     }
-    await this.supabaseClient!.auth.stopAutoRefresh();
-    this.logger.info(`Stopped auto refreshing Supabase token`);
+
+    if (this.refreshTokenInterval) {
+      clearInterval(this.refreshTokenInterval);
+    }
+
+    this.isClosed = true;
   }
 
   private async login(email: string, password: string) {
@@ -117,7 +170,7 @@ export class PeerBenchUploader extends AbstractUploader {
       return;
     }
 
-    return data.session.access_token;
+    return data;
   }
 
   private async signUp(email: string, password: string) {
@@ -136,6 +189,6 @@ export class PeerBenchUploader extends AbstractUploader {
       return;
     }
 
-    return data.session.access_token;
+    return data;
   }
 }
